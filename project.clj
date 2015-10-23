5e65bfdfdc63a8d64d2238918a9cda1b4a078c18
Sergio Alvarez-Napagao <tranchis@gmail.com>
2015-10-23 15:47:36 +0100

    Avoiding AOT because of the db macro

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject tranchis/photon-mongo "0.9.8"
+(defproject tranchis/photon-mongo "0.9.9"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :java-source-paths ["java"]
-  :aot :all
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [tranchis/photon-db "0.9.9"]
                  [tranchis/photon-config "0.9.8"]
