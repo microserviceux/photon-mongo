14009c62175583b968f7a8b48b75c90c733b2f59
Sergio Alvarez-Napagao <tranchis@gmail.com>
2015-10-23 14:50:55 +0100

    Move code from photon

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject tranchis/photon-mongo "0.9.7"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :java-source-paths ["java"]
+  :aot :all
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [tranchis/photon-db "0.9.9"]
+                 [tranchis/photon-config "0.9.8"]
+                 [congomongo "0.4.6"]
+                 [org.clojure/tools.logging "0.3.1"]])
---
+++ src/photon/db/mongo.clj
@@ -0,0 +1,60 @@
+(ns photon.db.mongo
+  (:require [photon.db :as db]
+            [photon.config :as conf]
+            [clojure.tools.logging :as log]
+            [somnium.congomongo :as m]))
+
+(def page-size 100)
+
+(defn mongo-conn []
+  (m/make-connection "photon" :host (:mongodb.host conf/config)))
+
+(def collection :events)
+
+(def db (ref nil))
+
+(defn m-mongo []
+  (if (nil? @db)
+    (let [new-db (mongo-conn)]
+      (dosync (alter db (fn [_] new-db)))
+      new-db)
+    @db))
+
+(def mongo (memoize m-mongo))
+
+(db/defdbplugin LocalMongoDB []
+  db/DB
+  (fetch [this stream-name id]
+    (m/with-mongo (mongo)
+      (m/fetch-one collection :where {:stream-name stream-name
+                                      :_id id})))
+  (delete! [this id]
+    (m/with-mongo (mongo)
+      (m/destroy! collection {:_id id})))
+  (delete-all! [this]
+    (m/with-mongo (mongo)
+      (m/destroy! collection {})))
+  (put [this data]
+    (m/with-mongo (mongo)
+      (m/insert! collection data)))
+  (search [this id] (db/fetch this {:$exists true} id))
+  (distinct-values [this k]
+    (m/with-mongo (mongo)
+      (m/distinct-values collection k)))
+  (store [this payload]
+    (m/with-mongo (mongo)
+      (m/insert! collection (merge payload {:_id (:local-id payload)}))))
+  (lazy-events [this stream-name date]
+    (db/lazy-events-page this stream-name date 0)) 
+  (lazy-events-page [this stream-name date page]
+    (m/with-mongo (mongo)
+      (let [l-date (if (string? date) (read-string date) date)
+            res (m/fetch collection :where {:stream-name stream-name}
+                         :skip (* page-size page) :limit page-size)]
+        (log/info "Calling mongo: " :where {:stream-name stream-name}
+                  :skip (* page-size page) :limit page-size)
+        (if (< (count res) 1)
+          []
+          (concat res
+                  (lazy-seq (db/lazy-events-page this stream-name l-date (inc page)))))))))
+
