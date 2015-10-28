723f3fc7867c47a60066c9fc7e11c5ab9438aa51
Sergio Alvarez-Napagao <tranchis@gmail.com>
2015-10-28 11:34:57 +0000

    Upgrade photon-db version

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject tranchis/photon-mongo "0.9.9"
+(defproject tranchis/photon-mongo "0.9.20"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :java-source-paths ["java"]
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [tranchis/photon-db "0.9.9"]
-                 [tranchis/photon-config "0.9.8"]
+                 [tranchis/photon-db "0.9.20"]
                  [congomongo "0.4.6"]
                  [org.clojure/tools.logging "0.3.1"]])
--- src/photon/db/mongo.clj
+++ src/photon/db/mongo.clj
@@ -1,54 +1,53 @@
 (ns photon.db.mongo
   (:require [photon.db :as db]
-            [photon.config :as conf]
             [clojure.tools.logging :as log]
             [somnium.congomongo :as m]))
 
 (def page-size 100)
 
-(defn mongo-conn []
-  (m/make-connection "photon" :host (:mongodb.host conf/config)))
+(defn mongo-conn [conf]
+  (m/make-connection "photon" :host (:mongodb.host conf)))
 
 (def collection :events)
 
 (def db (ref nil))
 
-(defn m-mongo []
+(defn m-mongo [conf]
   (if (nil? @db)
-    (let [new-db (mongo-conn)]
+    (let [new-db (mongo-conn conf)]
       (dosync (alter db (fn [_] new-db)))
       new-db)
     @db))
 
 (def mongo (memoize m-mongo))
 
-(db/defdbplugin LocalMongoDB []
+(db/defdbplugin LocalMongoDB [conf]
   db/DB
   (driver-name [this] "mongo")
   (fetch [this stream-name id]
-    (m/with-mongo (mongo)
+    (m/with-mongo (mongo conf)
       (m/fetch-one collection :where {:stream-name stream-name
                                       :_id id})))
   (delete! [this id]
-    (m/with-mongo (mongo)
+    (m/with-mongo (mongo conf)
       (m/destroy! collection {:_id id})))
   (delete-all! [this]
-    (m/with-mongo (mongo)
+    (m/with-mongo (mongo conf)
       (m/destroy! collection {})))
   (put [this data]
-    (m/with-mongo (mongo)
+    (m/with-mongo (mongo conf)
       (m/insert! collection data)))
   (search [this id] (db/fetch this {:$exists true} id))
   (distinct-values [this k]
-    (m/with-mongo (mongo)
+    (m/with-mongo (mongo conf)
       (m/distinct-values collection k)))
   (store [this payload]
-    (m/with-mongo (mongo)
+    (m/with-mongo (mongo conf)
       (m/insert! collection (merge payload {:_id (:local-id payload)}))))
   (lazy-events [this stream-name date]
     (db/lazy-events-page this stream-name date 0)) 
   (lazy-events-page [this stream-name date page]
-    (m/with-mongo (mongo)
+    (m/with-mongo (mongo conf)
       (let [l-date (if (string? date) (read-string date) date)
             res (m/fetch collection :where {:stream-name stream-name}
                          :skip (* page-size page) :limit page-size)]
